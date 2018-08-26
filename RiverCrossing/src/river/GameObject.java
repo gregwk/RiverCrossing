b774fdb2d448d0e30e566d41875f3240058abee5
Gregory Kulczycki <gregwk@vt.edu>
2018-08-26 17:55:01 -0400

    Removes refactoring comments

--- RiverCrossing/src/river/GameObject.java
+++ RiverCrossing/src/river/GameObject.java
@@ -2,20 +2,6 @@ package river;
 
 import river.GameEngine.Location;
 
-/*
- * (1) Introduce a field named sound of type String into this super class.
- *     Change the getSound method so it simply returns the sound field. Set
- *     sound values in subclass constructors as appropriate.
- *
- * (2) Create a constructor in this class that sets the name and sound to the
- *     empty string and sets the location to Location.START. Modify the
- *     constructors of the subclasses to call the superclass constructor -- by
- *     invoking super() -- and then override values as appropriate.
- *
- * (3) There is no reason for the name of the object to change, so remove the
- *     method setName.
- */
-
 public class GameObject {
 
     protected String name;
